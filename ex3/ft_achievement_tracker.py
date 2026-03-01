1f3dcc0ba99efd8c7e22689b1d484eb0072c3015
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-03-01 14:47:15 +0100

    done with ex3

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -2,9 +2,7 @@
 
 
 def demo() -> None:
-    print("=== Achievement Tracker System ===")
-
-    achievements = {
+    achvs = {
         "alice": [
             "first_blood",
             "pixel_perfect",
@@ -57,34 +55,34 @@ def demo() -> None:
         ],
     }
 
-    for player in achievements:
-        print(f"Player {player} acievements: {achievements[player]}")
+    player_achvs = {player: set(achvs[player]) for player in achvs}
+    distinct_achvs = \
+        set.union(*[player_achvs[player] for player in player_achvs])
+    common_achvs = \
+        set.intersection(*[player_achvs[player] for player in player_achvs])
+    alice = player_achvs["alice"]
+    bob = player_achvs["bob"]
+    all_achivements = [achv for player in achvs for achv in achvs[player]]
+    counts = {achv: 0 for achv in distinct_achvs}
+    for achv in all_achivements:
+        counts[achv] += 1
+    rares = {achv for achv in counts if counts[achv] == 1}
 
-    uniqs_per_player = [set(achievements[p]) for p in achievements]
-    uniqs_all = set.union(*uniqs_per_player)
-    common_all = set.intersection(*uniqs_per_player)
-    alice_bob = \
-        set.difference(set(achievements["alice"]), set(achievements["bob"]))
-
-    table = {}
-    for player in achievements:
-        for achievement in achievements[player]:
-            if achievement not in table:
-                table[achievement] = 1
-            else:
-                table[achievement] = 0
-    rares = [achievement for achievement in table if table[achievement] == 1]
+    print("=== Achievement Tracker System ===")
+    for player in achvs:
+        print(f"Player {player} acievements: {player_achvs[player]}")
 
     print("\n=== Achievement Analytics ===")
-    print(f"\nAll unique achievements: {uniqs_all}")
-    print(f"Total unique achievements: {len(uniqs_all)}")
+    print(f"All unique achievements: {distinct_achvs}")
+    print(f"Total unique achievements: {len(distinct_achvs)}")
 
-    print(f"\nCommon to all players:({list(common_all)})")
+    print("\nCommon to all players: ", end='')
+    print(f"({common_achvs if common_achvs != set() else '{}'})")
     print(f"Rare achievements (1 player): {rares}")
 
-    print(f"\nAlice vs Bob common: {alice_bob}")
-    print(f"Alice unique: {set(achievements['alice'])}")
-    print(f"Bob unique: {set(achievements['bob'])}")
+    print(f"\nAlice vs Bob common: {alice & bob}")
+    print(f"Alice unique: {alice - bob}")
+    print(f"Bob unique: {bob - alice}")
 
 
 if __name__ == "__main__":
